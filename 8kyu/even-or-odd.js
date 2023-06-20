dffce41245849bae036d07686dbc0041812a0061
Ike Emeche <thetrueepg@gmail.com>
2023-06-20 12:20:16 -0700

    Add coding challenge for 6-20-23 CodeWars.

---
+++ 8kyu/even-or-odd.js
@@ -0,0 +1,11 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+    //returns "Even" for even, "Odd" for odd.
+  
+    if(number % 2 === 0) {
+      return "Even"
+    } else {
+      return "Odd"
+    } 
+  }
