0dd04a53869494651a3674dec8c0b2357824ca58
Ike Emeche <thetrueepg@gmail.com>
2023-07-03 19:55:59 -0700

    Add coding challenge for 7-3-2023 CodeWars.

---
+++ 8kyu/beginner-lost-without-a-map.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+function maps(x){
+    return x.map(element => 2*element)
+  }
