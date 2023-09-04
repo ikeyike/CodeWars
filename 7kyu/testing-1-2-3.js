06e34e1e537866a77a244f6ce781f3ec5e553a03
Ike Emeche <thetrueepg@gmail.com>
2023-09-04 15:28:30 -0700

    Add coding challenge for 9-4-23 CodeWars.

---
+++ 7kyu/testing-1-2-3.js
@@ -0,0 +1,25 @@
+// DESCRIPTION:
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+var number=function(array){
+    if(array.length === 0){
+      return []
+    }
+    let result = []
+    let counter = 1
+    for(i = 0; i < array.length; i++){
+      result.push(counter + ': ' + array[i])
+      counter += 1
+    }
+    
+    return result
+}
